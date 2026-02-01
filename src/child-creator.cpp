99bc1d035049cc674350753eec51e4cafd32fea1
Jacek Skowron <dtsgseete@gmail.com>
2026-02-01 21:44:43 +0100

    Introduce macro for push

--- src/child-creator.cpp
+++ src/child-creator.cpp
@@ -25,11 +25,13 @@ struct CChildCreator : CChildCreatorIf {
 
 using UptrChCrIf = std::unique_ptr<CChildCreatorIf>;
 using VecOfUptrChCrIf = std::vector<UptrChCrIf>;
+#define PUSH_BACK(CHILD, EVENT) \
+        (mapPtr->push_back((UptrChCrIf)(new CChildCreator<CHILD>(EVENT))));
 void CChildCreatorIf::initMapWithCreators(void* mapVoidPtr) {
     VecOfUptrChCrIf* mapPtr = (VecOfUptrChCrIf*) mapVoidPtr;
     mapPtr->push_back(UptrChCrIf(nullptr)); /* exit */
-    mapPtr->push_back((UptrChCrIf)(new CChildCreator<CChild1>(1)));
-    mapPtr->push_back((UptrChCrIf)(new CChildCreator<CChild2>(2)));
-    mapPtr->push_back((UptrChCrIf)(new CChildCreator<CChild3>(3)));
-    mapPtr->push_back((UptrChCrIf)(new CChildCreator<CChild4>(4)));
+    PUSH_BACK(CChild1, 1);
+    PUSH_BACK(CChild2, 2);
+    PUSH_BACK(CChild3, 3);
+    PUSH_BACK(CChild4, 4);
 }
