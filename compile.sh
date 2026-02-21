ce9c8ace243b63489d7f3a737a878d8360874e14
My Name <me@example.com>
2026-02-21 22:06:24 +0100

    improve instruction: build and format

--- compile.sh
+++ compile.sh
@@ -1,5 +1,9 @@
 # in the higher level, where CMakeLists.txt is located, create directory 'build' and go to this directory
 cd build
-clear ; rm -rf * ; cmake ..
+clear ; rm -rf ~/cprogs/github/polimorfizm-ptr-move/build/* ; cmake ..
 clear ; make # alternative: cmake --build .
 clear ; ./polimorfizm-ptr-move 
+
+# to format:
+clang-format -i -style=chromium src/*.cpp
+clang-format -i -style=chromium src/include/*.hpp
