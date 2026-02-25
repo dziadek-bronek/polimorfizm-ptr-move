9f9422ac984ab674a2ef5a64ee1d2e89fcc88cb5
dtstgseete <dtstgseete@gmail.com>
2026-02-25 14:53:51 +0000

    check for destructor call

--- uts/src/ut.cpp
+++ uts/src/ut.cpp
@@ -1,7 +1,22 @@
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 
+#include "../../src/include/CChildren.hpp"
+#include <memory>
 
-TEST(ChangeMe,ChangeMeMore)
+struct CChild1Mock : CChild1
 {
+	public:
+		MOCK_METHOD(void, destr, (), (override));
+		virtual ~CChild1Mock() {destr(); }
+};
+
+TEST(MemoryManagement, Child1Destructor)
+{
+	using UPtrCChild1Mock = std::unique_ptr<CChild1Mock>;
+	{
+		std::unique_ptr<CChild1Mock> child1Mock(new CChild1Mock);
+		EXPECT_CALL(*child1Mock, destr());
+	}
+
 }
