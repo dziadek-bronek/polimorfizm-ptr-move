852822bb9bd11f0e7a8bea59c3f2e943546fb24f
dtstgseete <dtstgseete@gmail.com>
2026-04-11 19:23:57 +0000

    first commit code formated

---
+++ code-style.sh
@@ -0,0 +1,20 @@
+# clang-format -i -style=Microsoft ~/cprogs/github/polimorfizm-ptr-move/src/*.cpp
+# clang-format -i -style=Microsoft ~/cprogs/github/polimorfizm-ptr-move/src/include/*.hpp
+# clang-format -i -style=Microsoft ~/cprogs/github/polimorfizm-ptr-move/uts/src/*.cpp
+
+
+# x="Microsoft"
+# x="chromium"
+
+x="{$(cat /home/dtstgseete/cprogs/github/polimorfizm-ptr-move/code-style.txt  | tr '\n' ',')}"
+
+echo "$x"
+
+
+git ls-files | grep -E "\.(cpp|h|hpp|cc|cxx)$" | xargs clang-format -style="$x" -i
+
+
+
+#clang-format -i -style="$x" /home/dtstgseete/cprogs/github/polimorfizm-ptr-move/src/*.cpp
+#clang-format -i -style="$x" /home/dtstgseete/cprogs/github/polimorfizm-ptr-move/src/include/*.hpp
+#clang-format -i -style="$x" /home/dtstgseete/cprogs/github/polimorfizm-ptr-move/uts/src/*.cpp
--- polimorfizm-ptr-move.cpp
+++ polimorfizm-ptr-move.cpp
@@ -1,37 +1,48 @@
-#include<cstdio>
-#include<memory>
-#include<vector>
-#include<iostream>
+#include <cstdio>
+#include <iostream>
+#include <memory>
+#include <vector>
 
-struct CParent {
+struct CParent
+{
     virtual void action() = 0;
 };
 
-struct CParentCreator {
+struct CParentCreator
+{
     virtual std::unique_ptr<CParent> createNew() = 0;
 };
 
-template <typename T>
-struct CChildCreator : CParentCreator {
-    virtual std::unique_ptr<CParent> createNew() {return std::unique_ptr<CParent> (new T);}
+template <typename T> struct CChildCreator : CParentCreator
+{
+    virtual std::unique_ptr<CParent> createNew()
+    {
+        return std::unique_ptr<CParent>(new T);
+    }
 };
 
-std::vector<CParentCreator*> mapOfEventsAndChildren{0,0,0,0,0};
+std::vector<CParentCreator*> mapOfEventsAndChildren{0, 0, 0, 0, 0};
 
-template<typename CHILD>
-void mapChildCreatorToEvent(int event ) {
+template <typename CHILD> void mapChildCreatorToEvent(int event)
+{
     mapOfEventsAndChildren[event] = new CChildCreator<CHILD>;
 }
 
-std::unique_ptr<CParent> resolveEventToAChild(int event) {
-    if (0 == event) {
+std::unique_ptr<CParent> resolveEventToAChild(int event)
+{
+    if (0 == event)
+    {
         throw "Clean exit: event 'EXIT' in resolveEventToAChild";
     }
 
-    for(int i = 1; i < mapOfEventsAndChildren.size(); ++i) {
-        if(i == event) {
-            if(mapOfEventsAndChildren[i]) {
-                return std::unique_ptr<CParent>( ( (mapOfEventsAndChildren[i])-> createNew() ));
+    for (int i = 1; i < mapOfEventsAndChildren.size(); ++i)
+    {
+        if (i == event)
+        {
+            if (mapOfEventsAndChildren[i])
+            {
+                return std::unique_ptr<CParent>(
+                    ((mapOfEventsAndChildren[i])->createNew()));
             }
         }
     }
@@ -39,76 +50,105 @@ std::unique_ptr<CParent> resolveEventToAChild(int event) {
     throw "Exit on error: unknown event";
 }
 
-struct CInput {
+struct CInput
+{
     int indexOfCurrentEvent{0};
     std::unique_ptr<std::vector<int>> sequenceOfEvents;
     int currentEvent{0};
 
-    void init(std::unique_ptr<std::vector<int>> sequenceOfEvents_) {
+    void init(std::unique_ptr<std::vector<int>> sequenceOfEvents_)
+    {
         sequenceOfEvents = std::move(sequenceOfEvents_);
-	    setCurrentEvent();
+        setCurrentEvent();
     }
 
-    int getCurrentEvent() {
+    int getCurrentEvent()
+    {
         return currentEvent;
     }
 
-    int nextCurrentEvent() {
-	    ++indexOfCurrentEvent;
+    int nextCurrentEvent()
+    {
+        ++indexOfCurrentEvent;
         setCurrentEvent();
         return currentEvent;
     }
 
-    void setCurrentEvent() {
-        if (indexOfCurrentEvent < 0) {
+    void setCurrentEvent()
+    {
+        if (indexOfCurrentEvent < 0)
+        {
             throw "Exit on error: invalid index of event";
         }
 
-        if (sequenceOfEvents->size() <= indexOfCurrentEvent) {
+        if (sequenceOfEvents->size() <= indexOfCurrentEvent)
+        {
             throw "Clean exit: no more events";
         }
 
         currentEvent = (*sequenceOfEvents)[indexOfCurrentEvent];
-        if(0 == currentEvent) {
+        if (0 == currentEvent)
+        {
             throw "Clean exit: event 'EXIT' in sequenceOfEvents";
         }
     }
 };
 
-struct CChild1 : CParent {
-    virtual void action() { printf("behavior specific for child 1\n"); }
+struct CChild1 : CParent
+{
+    virtual void action()
+    {
+        printf("behavior specific for child 1\n");
+    }
 };
 
-struct CChild2 : CParent {
-    virtual void action() { printf("behavior specific for child 2\n"); }
+struct CChild2 : CParent
+{
+    virtual void action()
+    {
+        printf("behavior specific for child 2\n");
+    }
 };
 
-struct CChild3 : CParent {
-    virtual void action() { printf("behavior specific for child 3\n"); }
+struct CChild3 : CParent
+{
+    virtual void action()
+    {
+        printf("behavior specific for child 3\n");
+    }
 };
 
-void registerChildren() {
-        mapChildCreatorToEvent<CChild1>(1);
-        mapChildCreatorToEvent<CChild2>(2);
-        mapChildCreatorToEvent<CChild3>(3);
+void registerChildren()
+{
+    mapChildCreatorToEvent<CChild1>(1);
+    mapChildCreatorToEvent<CChild2>(2);
+    mapChildCreatorToEvent<CChild3>(3);
 }
 
-int main() {
-    try {
+int main()
+{
+    try
+    {
 
         CInput input;
-        input.init(std::unique_ptr<std::vector<int>> (new std::vector<int>{1,3,2,2,1,2,1,1,2,5}));
+        input.init(std::unique_ptr<std::vector<int>>(
+            new std::vector<int>{1, 3, 2, 2, 1, 2, 1, 1, 2, 5}));
 
         registerChildren();
 
-        for(int event = input.getCurrentEvent(); ;  event = input.nextCurrentEvent() /* input++*/ ) {
+        for (int event = input.getCurrentEvent();;
+             event = input.nextCurrentEvent() /* input++*/)
+        {
             resolveEventToAChild(event)->action();
         }
-
-    } catch(const char* result) {
+    }
+    catch (const char* result)
+    {
         printf("%s.\n", result);
         return 0;
-    } catch(...) {
+    }
+    catch (...)
+    {
         printf("FATAL ERROR! Unhandled exception.");
         return 1;
     }
